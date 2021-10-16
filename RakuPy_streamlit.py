6fc76105c3c32978ff0c395effa4a236399ed850
GillesNicolet <80528589+GillesNicolet@users.noreply.github.com>
2021-10-16 20:30:49 +0200

    Update RakuPy_streamlit.py

--- RakuPy_streamlit.py
+++ RakuPy_streamlit.py
@@ -14,19 +14,35 @@ liste_choix = ['Le projet',
                'Dataset',
                'Le modèle',
                'Demo']
-choix = st.sidebar.radio('Menu',liste_choix)
+choix = st.sidebar.radio('',liste_choix)
 
 
-
-
+if choix==liste_choix[0]:
+    st.title('Le projet RakuPy')
+    st.subheader("Classification automatique d'articles e-commerce")
 
 #st.write('')
 
+if choix==liste_choix[1]:
+    st.title('Dataset')
+
 #st.dataframe('')
+
+    
+if choix==liste_choix[2]:
+    st.title('Le modèle')
+ 
+    
+if choix==liste_choix[3]:
+    st.title('Demo')
 
 #st.cache()
 #def train_model():
     
 #    return
+    
+
+
+
 
 
