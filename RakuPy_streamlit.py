834826a395119097d1bce39ec14e7858541ab9b8
GillesNicolet <80528589+GillesNicolet@users.noreply.github.com>
2021-11-16 10:29:34 +0100

    Update RakuPy_streamlit.py

--- RakuPy_streamlit.py
+++ RakuPy_streamlit.py
@@ -14,14 +14,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 #import tensorflow as tf
 
-
-from tensorflow import keras
-from keras.preprocessing.sequence import pad_sequences
-from keras.models import load_model
-
-
-#from tensorflow.keras.preprocessing.sequence import pad_sequences
-#from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import load_model
 
 
 #from sklearn.model_selection import train_test_split
